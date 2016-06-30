f607db9174a977b58fa1b173b803823769da1abe
Loveorigami <loveorigami@mail.ru>
2016-06-30 19:50:50 +0300

    move config to frontend

--- config/main.php
+++ config/main.php
@@ -1,19 +1,4 @@
 <?php
-
+// config placed in frontend/comfig/main.php
 return [
-    'modules' => [
-        'vote' => [
-            'class' => 'lo\modules\vote\Module',
-            'allowGuests' => false, // if true will check IP, otherwise - UserID
-            'allowChangeVote' => true, // if true vote can be changed
-            'models' => [ // matching model names with whatever unique integer ID
-                3 => [
-					'modelName' => 'lo\modules\love\models\Aphorism',
-					'allowGuests' => false,
-					'allowChangeVote' => true
-				] 
-                //'lo\modules\love\models\Aphorism' => 3, // or array with 'id' key
-            ],
-        ],
-    ],
 ];
